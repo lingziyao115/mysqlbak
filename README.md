c501715cc421c7d82adfa71779c8bbdde4bbea24
eric <ericling@xin3wei.com>
2017-11-03 14:58:11 +0800

    Fixed bug:配置文件使用绝对路径

--- README.md
+++ README.md
@@ -9,19 +9,25 @@ backup_example.conf ---- The sample configure file
 
 backup.conf ---- Configure file
 
-crontab.log ---------- Cron log file
+crontab.log ---------- Crond output log
 
 ## How To Use
 
 ### Configure
 
-Copy the sample configure file as formal configure file (named as `backup.conf`)
+Copy the sample configure file as formal configure file (named as `backup.conf`).
 
-Then modify the mysql connect configure and backup settings.
+Then modify the mysql connection configure and backup settings.
 
 ### Auto Run The Script
 
-Use crontab on Linux.
+Use crond on Linux, type the following command
+
+```shell
+crontab -e
+```
+
+Then edit this file as `vi`, the following instruction will execute at 12:30 and 18:30 every day.
 
 `30 12,18 * * * /bin/bash /path/to/mysqlbak/mysqlbak.sh >> /path/to/mysqlbak/crontab.log`
 
@@ -42,14 +48,23 @@ backup_example.conf ---- 样例配置文件
 
 backup.conf ---- 配置文件
 
-crontab.log ---------- Cron 日志文件
+crontab.log ---------- Crond 输出的日志
 
 ## 使用方法
 
 ### 配置
 将样例配置文件复制一份，命名为 backup.conf，修改其中的数据库连接配置和备份配置。
 
-### 设置 crontab 自动执行
+### 设置 crond 自动执行
+
+执行以下命令
+
+```shell
+crontab -e
+```
+
+像使用 `vi` 一样编辑这个文件，以下命令将在每天 12:30 和 18:30 各执行一次
+
 `30 12,18 * * * /bin/bash /path/to/mysqlbak/mysqlbak.sh >> /path/to/mysqlbak/crontab.log`
 
 ## 作者
--- mysqlbak.sh
+++ mysqlbak.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CONFIG_FILE=backup.conf
+CONFIG_FILE_NAME=backup.conf
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-DEFAULTS_EXTRA_FILE=$SCRIPT_DIR/$CONFIG_FILE
+CONFIG_FILE=$SCRIPT_DIR/$CONFIG_FILE_NAME
 
 DB_NAMES=$(grep db_names $CONFIG_FILE | cut -d= -f2)
 DB_NAMES_ARR=(${DB_NAMES//,/ })
@@ -13,8 +13,8 @@ MYSQLDUMP_CMD=$(grep mysqldump_cmd $CONFIG_FILE | cut -d= -f2)
 DATE=`date +%Y%m%d`
 TIME=`date +%H%M%S`
 
-echo "[$DATE$TIME]"
-echo "Starting ..."
+echo "[$DATE $TIME]"
+echo "Backuping ..."
 for DB_NAME in ${DB_NAMES_ARR[@]}
 do
     TODAY_DIR=$BACKUP_DIR/$DATE
@@ -23,10 +23,18 @@ do
     fi
 
     echo -n "  BACKUP $DB_NAME ... "
-    $MYSQLDUMP_CMD --defaults-extra-file=$DEFAULTS_EXTRA_FILE --triggers --routines --events $DB_NAME | gzip > $TODAY_DIR/$DB_NAME.$TIME.sql.gz
+    $MYSQLDUMP_CMD --defaults-extra-file=$CONFIG_FILE --triggers --routines --events $DB_NAME | gzip > $TODAY_DIR/$DB_NAME.$TIME.sql.gz
     echo "[OK]"
 done
-echo "End!"
+echo "Backup End!"
 
 # delete the backup files before n days
-find $BACKUP_DIR -type d -ctime +$BACKUP_DAYS | xargs rm -rf > /dev/null 2>&1
+# 删除 n 天前的备份数据
+DELETED_BACKUP_DIRS=$(find $BACKUP_DIR -type d -ctime +$BACKUP_DAYS)
+echo "Deleting old data ..."
+for DELETED_DIR in $DELETED_BACKUP_DIRS
+do
+    rm -rf $DELETED_DIR > /dev/null 2>&1
+    echo "  $DELETED_DIR ... [DONE]"
+done
+echo "Delete End!"
